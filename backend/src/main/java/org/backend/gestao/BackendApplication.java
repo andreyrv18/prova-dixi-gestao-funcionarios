e3557d381a30081162fa11deb1cf9a0d5604363c
Andreyrvdev <andreyrv18@gmail.com>
2026-06-22 00:58:38 -0300

    Update: (Backend) - Ajusta cors para aceitar requisição frontend no modo dev

--- backend/src/main/java/org/backend/gestao/BackendApplication.java
+++ backend/src/main/java/org/backend/gestao/BackendApplication.java
@@ -2,12 +2,14 @@ package org.backend.gestao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
@@ -16,6 +18,6 @@ public class BackendApplication {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return "Hello from Backend!";
     }
 }
--- backend/src/main/java/org/backend/gestao/CorsConfig.java
+++ backend/src/main/java/org/backend/gestao/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 public class CorsConfig {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS");
     }
 }
