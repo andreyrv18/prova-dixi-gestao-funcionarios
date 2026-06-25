a7405efff4e4a1c3122d7e338e56cdbe9b1ba1d8
Andreyrvdev <andreyrv18@gmail.com>
2026-06-25 02:15:54 -0300

    Feature: (Frontend) - react router no modo de dados usando objetos js para fazer as rotas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider,} from "react-router";
+import App from "./App.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import FuncionariosPage from "./pages/FuncionariosPage.tsx";
+import DepartamentoPage from "./pages/DepartamentoPage.tsx";
+import CargoPage from "./pages/CargoPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import {fetchAPI} from "./service.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: "/home",
+                loader: async () => {
+                    return {records: await fetchAPI()};
+                },
+                element: <HomePage/>,
+
+
+            },
+            {
+                path: "/funcionario", element: <FuncionariosPage/>
+            },
+            {
+                path: "/departamento", element: <DepartamentoPage/>
+            },
+            {
+                path: "/cargo", element: <CargoPage/>
+            }
+
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
