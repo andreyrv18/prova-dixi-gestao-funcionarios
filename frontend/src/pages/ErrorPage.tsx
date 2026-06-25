e7953096df405f58e8208de36982a14b11337c9c
Andreyrvdev <andreyrv18@gmail.com>
2026-06-25 02:14:13 -0300

    Feature: (Frontend) - Pagina de Erro

---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import imagem from '../assets/404.webp'
+import {Link} from "react-router-dom";
+function ErrorPage() {
+    return (
+        <div style={{ display: 'grid', gap:"10px", fontSize:"40px", alignItems: 'center', height:"75vh", overflow:"hidden" }}>
+
+            <img src={imagem} alt="relógio ponto erro" ></img>
+            <Link to="/">Voltar a página Inicial</Link>
+        </div>
+    );
+}
+
+export default ErrorPage;
