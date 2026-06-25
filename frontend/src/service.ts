7d641569e163d3ebd1bae586dd4418cdaec10d56
Andreyrvdev <andreyrv18@gmail.com>
2026-06-25 02:16:42 -0300

    feat: (Frontend) - valida resposta da API

--- frontend/src/service.ts
+++ frontend/src/service.ts
@@ -1,4 +1,7 @@
 export const fetchAPI = async (): Promise<string> => {
     const response = await fetch (`http://localhost:8080/hello`);
+    if(!response.ok) {
+        throw new Error('Falha ao buscar');
+    }
     return await response.text();
 }
