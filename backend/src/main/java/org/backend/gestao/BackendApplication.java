71247b051cd2e6fb7925af3f8d67c2fb6a56ad77
Andreyrvdev <andreyrv18@gmail.com>
2026-07-03 02:20:24 -0300

    fix: (Backend) Main - com nova api URI

--- backend/src/main/java/org/backend/gestao/BackendApplication.java
+++ backend/src/main/java/org/backend/gestao/BackendApplication.java
@@ -2,14 +2,12 @@ package org.backend.gestao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
