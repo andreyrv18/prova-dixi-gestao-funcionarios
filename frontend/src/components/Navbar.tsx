0cca2dc1588ad629a72f484e51dc16b553282d59
Andreyrvdev <andreyrv18@gmail.com>
2026-07-07 01:35:58 -0300

    feat: (Frontend) - altera Navbar para aceitar os locales e também o css usando os tokens

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,24 @@
 import {Link} from "react-router-dom";
 import styles from "./Navbar.module.css"
-import {MdBadge, MdMeetingRoom, MdPerson  } from "react-icons/md";
+import {MdBadge, MdMeetingRoom, MdPerson} from "react-icons/md";
 import LogoDixi from "../assets/Union.svg"
+import {ptBR} from "../locales/pt-BR";
+
+
 function Navbar() {
     return (
         <div className={styles.menu}>
 
-            <img className={styles.link}  src={LogoDixi} alt="logo dixi"></img>
-            <hr  className={styles.linha}/>
-            {/*<Link className={styles.link} to="/home">home</Link>*/}
-            <Link className={styles.link} to="/funcionario"><MdPerson size={40}/> Funcionário</Link>
-            <hr  className={styles.linha}/>
+            <img className={styles.link} src={LogoDixi} alt="logo dixi"></img>
+            <hr className={styles.linha}/>
 
-            <Link className={styles.link} to="/cargo"><MdBadge size={40}/> Cargo</Link>
+            <Link className={styles.link} to="/funcionario"><MdPerson size={40}/>{ptBR.navbar.funcionario}</Link>
+            <hr className={styles.linha}/>
 
-            <hr  className={styles.linha}/>
-            <Link className={styles.link} to="/departamento"><MdMeetingRoom size={40} />Departamento</Link>
+            <Link className={styles.link} to="/cargo"><MdBadge size={40}/>{ptBR.navbar.cargo}</Link>
+
+            <hr className={styles.linha}/>
+            <Link className={styles.link} to="/departamento"><MdMeetingRoom size={40}/>{ptBR.navbar.departamento}</Link>
         </div>
     );
 }
