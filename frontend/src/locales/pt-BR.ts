6372a670713037e8d13c23b28227c9974dff388b
Andreyrvdev <andreyrv18@gmail.com>
2026-07-07 23:18:53 -0300

    feat: (Frontend) - tabela em portugues

--- frontend/src/locales/pt-BR.ts
+++ frontend/src/locales/pt-BR.ts
@@ -3,43 +3,62 @@ interface Dicionario {
         funcionario: string;
         cargo: string;
         departamento: string;
-    },
+    };
+    tabela: {
+        cabecalho: {
+            editar: string;
+            nome: string;
+            cpf: string;
+            descricao: string;
+            codigo: string;
+        };
+        corpo: string;
+    };
     funcionario: {
-        listarFundionarios:
-            {
-                titulo: string;
-                subtitulo: string;
-            },
+        listarFundionarios: {
+            titulo: string;
+
+            subtitulo: string;
+            cardSubtitulo: string;
+        };
         editarFuncionario: {
             titulo: string;
             subtitulo: string;
-        },
-    },
+        };
+    };
     paginaCargo: {
         titulo: string;
         subtitulo: string;
-    },
+    };
     paginaDepartamento: {
         titulo: string;
         subtitulo: string;
-    }
-
+    };
 }
 
 export const ptBR: Dicionario = {
-
     navbar: {
         funcionario: "Funcionario",
         cargo: "Cargo",
         departamento: "Departamento",
     },
+    tabela: {
+        cabecalho: {
+            editar: "Editar",
+            nome: "Nome",
+            cpf: "Cpf",
+            descricao: "Descrição",
+            codigo: "Código",
+        },
+        corpo: "",
+    },
     funcionario: {
-        listarFundionarios:
-            {
-
-                titulo: "Funcionários",
-                subtitulo: "Veja os funcionários cadastrados no sistema.",
-            },
+        listarFundionarios: {
+            titulo: "Funcionários",
+            subtitulo: "Veja os funcionários cadastrados no sistema.",
+            cardSubtitulo:
+                "Clique para ver os vínculos de empresa do funcionário",
+        },
         editarFuncionario: {
             titulo: "Editar Funcionário",
             subtitulo: "Altere as informaçoes deste funcionário",
@@ -53,6 +72,5 @@ export const ptBR: Dicionario = {
     paginaDepartamento: {
         titulo: "Departamentos",
         subtitulo: "Veja os departamentos cadastrados no sistema.",
-    }
-
-}
+    },
+};
