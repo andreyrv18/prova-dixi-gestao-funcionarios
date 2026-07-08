820baa59f9fcf2b846f488072c3ea44e074b22fe
Andreyrvdev <andreyrv18@gmail.com>
2026-07-08 01:16:10 -0300

    fix: (Frontend) Cargo, Departamento - Insere cabacalho e card genericos e ajusta conforme a pagina

--- frontend/src/pages/CargoPage.tsx
+++ frontend/src/pages/CargoPage.tsx
@@ -1,10 +1,54 @@
-import { ptBR } from "../locales/pt-BR.ts";
+import PaginaCabacalho from "../components/PaginaCabacalho.tsx";
+import {ptBR} from "../locales/pt-BR.ts";
+import {Card} from "../components/Card.tsx";
+import Input from "../components/Input.tsx";
+import {useState} from "react";
 
 function CargoPage() {
+    const [descricao, setDescricao] = useState("");
+    const [codigo, setCodigo] = useState("");
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
-            <span className="titulo">{ptBR.paginaCargo.titulo}</span>
-            <span className="tituloModal">{ptBR.paginaCargo.subtitulo}</span>
+            <PaginaCabacalho
+                titulo={ptBR.paginaCargo.listar.titulo}
+                subtitulo={ptBR.paginaCargo.listar.subtitulo}
+                nomeBotao={ptBR.botao.novoCargo}
+            />
+            <Card
+                inputs={
+                    <>
+                        <Input
+                            dataTest={ptBR.paginaCargo.input.dataTest.descricao}
+                            id="cargo-descricao"
+                            labelName={ptBR.paginaCargo.input.label}
+                            name={ptBR.paginaCargo.input.nameDescricao}
+                            onInputChange={e => setDescricao(e.target.value)}
+                            value={descricao}
+                            type="text"
+                            placeholder={
+                                ptBR.paginaCargo.input.placeholderDescricao
+                            }
+                        />
+                        <Input
+                            dataTest={ptBR.paginaCargo.input.dataTest.codigo}
+                            id="codigo"
+                            labelName={ptBR.paginaCargo.input.nameCodigo}
+                            name={ptBR.paginaCargo.input.nameCodigo}
+                            onInputChange={e => setCodigo(e.target.value)}
+                            value={codigo}
+                            type="text"
+                            placeholder={
+                                ptBR.paginaCargo.input.placeholderCodigo
+                            }
+                        />
+                    </>
+                }
+                tabelaCabecalho={{
+                    editar: ptBR.tabela.cabecalho.editar,
+                    nome: ptBR.tabela.cabecalho.nome,
+                    cpf: ptBR.tabela.cabecalho.cpf,
+                }}
+            ></Card>
         </div>
     );
 }
--- frontend/src/pages/DepartamentoPage.tsx
+++ frontend/src/pages/DepartamentoPage.tsx
@@ -1,12 +1,60 @@
+import PaginaCabacalho from "../components/PaginaCabacalho.tsx";
 import { ptBR } from "../locales/pt-BR.ts";
+import { Card } from "../components/Card.tsx";
+import Input from "../components/Input.tsx";
+import { useState } from "react";
 
 function DepartamentoPage() {
+    const [descricao, setDescricao] = useState("");
+    const [codigo, setCodigo] = useState("");
+
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
-            <span className="titulo">{ptBR.paginaDepartamento.titulo}</span>
-            <span className="tituloModal">
-                {ptBR.paginaDepartamento.subtitulo}
-            </span>
+            <PaginaCabacalho
+                titulo={ptBR.paginaDepartamento.listar.titulo}
+                subtitulo={ptBR.paginaDepartamento.listar.subtitulo}
+                nomeBotao={ptBR.botao.novoDepartamento}
+            />
+            <Card
+                inputs={
+                    <>
+                        <Input
+                            dataTest={
+                                ptBR.paginaDepartamento.input.dataTest.descricao
+                            }
+                            id="cargo-descricao"
+                            labelName={ptBR.paginaDepartamento.input.label}
+                            name={ptBR.paginaDepartamento.input.nameDescricao}
+                            onInputChange={e => setDescricao(e.target.value)}
+                            value={descricao}
+                            type="text"
+                            placeholder={
+                                ptBR.paginaDepartamento.input
+                                    .placeholderDescricao
+                            }
+                        />
+                        <Input
+                            dataTest={
+                                ptBR.paginaDepartamento.input.dataTest.codigo
+                            }
+                            id="codigo"
+                            labelName={ptBR.paginaDepartamento.input.nameCodigo}
+                            name={ptBR.paginaDepartamento.input.nameCodigo}
+                            onInputChange={e => setCodigo(e.target.value)}
+                            value={codigo}
+                            type="text"
+                            placeholder={
+                                ptBR.paginaDepartamento.input.placeholderCodigo
+                            }
+                        />
+                    </>
+                }
+                tabelaCabecalho={{
+                    editar: ptBR.tabela.cabecalho.editar,
+                    nome: ptBR.tabela.cabecalho.nome,
+                    cpf: ptBR.tabela.cabecalho.cpf,
+                }}
+            ></Card>
         </div>
     );
 }
