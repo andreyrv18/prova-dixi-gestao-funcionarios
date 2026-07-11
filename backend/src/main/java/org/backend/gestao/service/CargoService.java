179b8109eadc03178b608e3248fa9a7e4b5d28eb
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 01:06:27 -0300

    feat: (Backend) Cargos - cria metodo para buscar todos e para criar cargos

--- backend/src/main/java/org/backend/gestao/service/CargoService.java
+++
@@ -1,8 +0,0 @@
-package org.backend.gestao.service;
-
-public class CargoService {
-
-    public String consultar() {
-        return "Cargos do service";
-    }
-}
---
+++ backend/src/main/java/org/backend/gestao/service/CargosService.java
@@ -0,0 +1,35 @@
+package org.backend.gestao.service;
+
+import org.backend.gestao.model.Cargos;
+import org.backend.gestao.repository.CargosRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CargosService {
+    private static final Logger log = LoggerFactory.getLogger(CargosService.class);
+
+    private final CargosRepository cargosRepository;
+
+    public CargosService(CargosRepository cargosRepository) {
+        this.cargosRepository = cargosRepository;
+    }
+
+    public List<Cargos> findAll() {
+        return cargosRepository.findAll();
+    }
+
+    public Cargos criarCargos(Cargos cargos) {
+        boolean cargoExist = cargosRepository.existsByCodigoDoCargo(cargos.getCodigoDoCargo());
+        if (cargoExist) {
+            log.warn("Usuario tentou cadastrar um cargo com código duplicado {}",  cargos.getCodigoDoCargo());
+            throw new IllegalArgumentException("Cógido do cargo dever ser unico");
+        }
+
+        log.info("Novo cargo criado com código: {}",  cargos.getCodigoDoCargo());
+        return cargosRepository.save(cargos);
+    }
+}
