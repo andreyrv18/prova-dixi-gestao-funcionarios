612dd0c76eaa80530cb8ceb533cfe45d7a7593fd
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 19:58:53 -0300

    feat: (Frontend) Interfaces - cria arquivo de interfaces para reutilização

---
+++ frontend/src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface Funcionarios {
+    cpf: string;
+    nome: string;
+}
+
+export interface Cargos {
+    codigoDoCargo: number;
+    descricaoDoCargo: string;
+}
+
+export interface Departamentos {
+    codigoDepartamento: number;
+    descricaoDoDepartamento: string;
+}
