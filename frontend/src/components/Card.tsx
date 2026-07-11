572f0de49a5581086dbfdf3be75377e5c180007e
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 19:58:32 -0300

    fix: (Frontend) Componentes - agora o card é Card genérico e reutilizável

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,29 +2,30 @@ import style from "./card.module.css";
 import React, { type PropsWithChildren } from "react";
 import Tabela from "./Tabela.tsx";
 
-interface CardType {
-    tabelaCabecalho?: {
-        editar: string;
-        nome?: string;
-        cpf?: string;
-        codigo?: string;
-        descricao?: string;
+interface CardProps {
+    tabelaConfig?: {
+        textoEditar: string;
+        tituloColuna1: string;
+        tituloColuna2: string;
+        routeId: string;
+        chavesDeAcesso: string[];
+        // rotaEdicao: string;
     };
     inputs?: React.ReactNode;
 }
 
-export function Card({ inputs, tabelaCabecalho }: PropsWithChildren<CardType>) {
+export function Card({ inputs, tabelaConfig }: PropsWithChildren<CardProps>) {
     return (
         <section className={style.card}>
             {inputs && <section className={style.cardInputs}>{inputs}</section>}
 
-            {tabelaCabecalho && (
+            {tabelaConfig && (
                 <Tabela
-                    nome={tabelaCabecalho.nome}
-                    cpf={tabelaCabecalho.cpf}
-                    codigo={tabelaCabecalho.codigo}
-                    descricao={tabelaCabecalho.descricao}
-                    editar={tabelaCabecalho.editar}
+                    textoEditar={tabelaConfig.textoEditar}
+                    tituloColuna1={tabelaConfig.tituloColuna1}
+                    tituloColuna2={tabelaConfig.tituloColuna2}
+                    routeId={tabelaConfig.routeId}
+                    chavesDeAcesso={tabelaConfig.chavesDeAcesso}
                 ></Tabela>
             )}
         </section>
