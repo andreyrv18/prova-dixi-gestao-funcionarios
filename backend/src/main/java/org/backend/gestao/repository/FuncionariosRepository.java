3c808a3ae43367ff6d9a1b8e80e0263bb9fb41ac
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 19:55:32 -0300

    Feat: (Backend)  Repository - Adiciona Paginação

--- backend/src/main/java/org/backend/gestao/repository/FuncionariosRepository.java
+++ backend/src/main/java/org/backend/gestao/repository/FuncionariosRepository.java
@@ -2,8 +2,11 @@ package org.backend.gestao.repository;
 
 import org.backend.gestao.model.Funcionarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FuncionariosRepository extends JpaRepository<Funcionarios, Long> {
+public interface FuncionariosRepository extends PagingAndSortingRepository<Funcionarios, Long>,JpaRepository<Funcionarios, Long> {
 
   boolean existsByCpf(String CPF);
+
+
 }
