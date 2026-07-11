ab323cb1afc47599d848db78d350866c97f16f00
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 19:58:05 -0300

    fix: (Frontend) Componentes - usa AppIcons.ts e arquivos de rotas assim mantendo um componente generico e limpo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import {Link} from "react-router-dom";
 import styles from "./navbar.module.css";
 import LogoDixi from "../assets/Union.svg";
 import {ptBR} from "../locales/pt-BR";
-import {AppIcons} from "./AppIcons.ts";
+import {AppIcons} from "../util/AppIcons.ts";
+import {rotas} from "../util/rotas.ts";
 
 function Navbar() {
     return (
@@ -10,20 +11,20 @@ function Navbar() {
             <img className={styles.link} src={LogoDixi} alt="logo dixi"></img>
             <hr className={styles.linha} />
 
-            <Link className={styles.link} to="/funcionario">
+            <Link className={styles.link} to={rotas.funcionarios.listar}>
                 <AppIcons.Funcionario className={styles.icons} />
                 {ptBR.navbar.funcionario}
             </Link>
             <hr className={styles.linha} />
 
-            <Link className={styles.link} to="/cargo">
+            <Link className={styles.link} to={rotas.cargos.listar}>
                 <AppIcons.Cargo className={styles.icons} />
                 {ptBR.navbar.cargo}
             </Link>
 
             <hr className={styles.linha} />
 
-            <Link className={styles.link} to="/departamento">
+            <Link className={styles.link} to={rotas.departamentos.listar}>
                 <AppIcons.Departamento className={styles.icons} />
                 {ptBR.navbar.departamento}
             </Link>
