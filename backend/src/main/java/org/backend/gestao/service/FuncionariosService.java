aab9f30077f7d30daffff3809cd2c926eb1bf6c0
Andreyrvdev <andreyrv18@gmail.com>
2026-07-11 01:07:02 -0300

    feat: (Backend) Funcionarios - cria metodo para buscar todos e para criar validando dados do funcionario

--- backend/src/main/java/org/backend/gestao/service/FuncionariosService.java
+++ backend/src/main/java/org/backend/gestao/service/FuncionariosService.java
@@ -1,4 +1,51 @@
 package org.backend.gestao.service;
 
+import org.backend.gestao.model.Funcionarios;
+import org.backend.gestao.repository.FuncionariosRepository;
+import org.backend.gestao.util.ValidaCPF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FuncionariosService {
+    private static final Logger log = LoggerFactory.getLogger(FuncionariosService.class);
+    private final FuncionariosRepository funcionariosRepository;
+
+    public FuncionariosService(FuncionariosRepository funcionariosRepository) {
+        this.funcionariosRepository = funcionariosRepository;
+    }
+
+    public List<Funcionarios> findAllFuncionarios() {
+
+        return funcionariosRepository.findAll();
+    }
+
+    public Funcionarios criarFuncionario(Funcionarios funcionarios) {
+        boolean cpfExiste = funcionariosRepository.existsByCpf(funcionarios.getCpf());
+        if (cpfExiste) {
+            log.warn("Usuário tentou cadastrar um funcionário com CPF existente {}", funcionarios.getCpf());
+            throw new IllegalArgumentException("CPF do funcionário deve ser único");
+        }
+
+        if (funcionarios.getCpf().length() != 11) {
+
+            throw new IllegalArgumentException("Cpf deve conter 11 dígitos");
+
+        }
+
+        if (funcionarios.getNome().length() < 3) {
+            throw new IllegalArgumentException("Nome deve ter mais de 3 dígitos");
+        }
+
+
+        if (!ValidaCPF.isCpf(funcionarios.getCpf())) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+
+        return funcionariosRepository.save(funcionarios);
+
+    }
 }
