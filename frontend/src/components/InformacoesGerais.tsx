28c850ad6970d72b0a6f4e19e875a4778108948a
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 03:48:05 -0300

    fix: (Frontend) components: inforamações gerais agora é geneciro

--- frontend/src/components/InformacoesGerais.tsx
+++ frontend/src/components/InformacoesGerais.tsx
@@ -1,36 +1,15 @@
-import {Card} from "./Card.tsx";
-import Input from "./Input.tsx";
-import {ptBR} from "../locales/pt-BR.ts";
+import { Card } from "./Card.tsx";
+import { ptBR } from "../locales/pt-BR.ts";
 import styles from "./informacoesGerais.module.css";
+import type { ReactNode } from "react";
 
-function InformacoesGerais() {
+function InformacoesGerais({ children }: { children: ReactNode }) {
     return (
         <>
             <Card>
                 <h1>{ptBR.card.titulo}</h1>
 
-                <section className={styles.inputs}>
-                    <Input
-                        dataTest={"a"}
-                        id={ptBR.input.nomeDoFuncionario}
-                        labelName={ptBR.input.nomeDoFuncionario}
-                        name={ptBR.input.nomeDoFuncionario}
-                        onInputChange={() => ""}
-                        value={""}
-                        type={"text"}
-                        placeholder={ptBR.input.insiraONomeDoFuncionario}
-                    />
-                    <Input
-                        dataTest={"b"}
-                        id={ptBR.input.cpf}
-                        labelName={ptBR.input.cpf}
-                        name={ptBR.input.cpf}
-                        onInputChange={() => ""}
-                        value={""}
-                        type={"text"}
-                        placeholder={ptBR.input.mascaraCPF}
-                    />
-                </section>
+                <section className={styles.inputs}>{children}</section>
             </Card>
         </>
     );
