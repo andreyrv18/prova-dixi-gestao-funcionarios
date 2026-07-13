c3f8c2d118ac1c61de7a72729c226539bc11f638
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 03:49:21 -0300

    fix: (Frontend) : departamentos busca por Id

--- frontend/src/services/DepartamentosService.ts
+++ frontend/src/services/DepartamentosService.ts
@@ -1,5 +1,5 @@
-import type { IDepartamentos } from "../interfaces";
-import { rotas } from "../util/rotas.ts";
+import type {IDepartamentos} from "../interfaces";
+import {rotas} from "../util/rotas.ts";
 
 export const GetDepartamentos = async (): Promise<IDepartamentos[]> => {
     const response = await fetch(rotas.api.departamentos, {
@@ -10,6 +10,16 @@ export const GetDepartamentos = async (): Promise<IDepartamentos[]> => {
     return dataJson;
 };
 
+export const GetDepartamentoById = async (
+    id: string,
+): Promise<IDepartamentos> => {
+    const response = await fetch(`${rotas.api.departamentos}/${id}`, {
+        method: "GET",
+    });
+    if (!response.ok) throw await response.json();
+    return response.json();
+};
+
 export const PostDepartamentos = async (payload: object) => {
     const response = await fetch(rotas.api.departamentos, {
         method: "POST",
@@ -17,5 +27,18 @@ export const PostDepartamentos = async (payload: object) => {
         body: JSON.stringify(payload),
     });
 
+    return await response.json();
+};
+
+export const PutDepartamento = async (id: string, departamentoDTO: object) => {
+    const response = await fetch(`${rotas.api.departamentos}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(departamentoDTO),
+    });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
     return await response.json();
 };
