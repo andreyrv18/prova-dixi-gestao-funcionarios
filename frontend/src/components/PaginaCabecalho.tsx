94da01b6b72e6a5b4e51c35ab4720898fcdba759
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:29:30 -0300

    fix: (Frontend) Componentes - PaginaCabecalho.tsx agora fica generico podendo ser usado nas telas de editar

---
+++ frontend/src/components/PaginaCabecalho.tsx
@@ -0,0 +1,44 @@
+import {ptBR} from "../locales/pt-BR.ts";
+import {AppIcons} from "../util/AppIcons.ts";
+import style from "./paginaCabecalho.module.css";
+import Button from "./Button.tsx";
+
+interface PaginaCabacalhoProps {
+    titulo: string;
+    subtitulo: string;
+    nomeBotao?: string;
+}
+
+function PaginaCabecalho({
+    titulo,
+    subtitulo,
+    nomeBotao,
+}: PaginaCabacalhoProps) {
+    return (
+        <div className={style.cabecalho}>
+            <div className={style.titulos}>
+                <span className="tituloPagina">{titulo}</span>
+                <span className="subtitulo">{subtitulo}</span>
+            </div>
+
+            {nomeBotao ? (
+                <div className={style.botoes}>
+                    <Button
+                        Icon={AppIcons.Download}
+                        type="button"
+                        name={ptBR.botao.baixarRelatorio}
+                    />
+                    <Button
+                        Icon={AppIcons.Adicionar}
+                        type="button"
+                        name={nomeBotao}
+                    />
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+}
+
+export default PaginaCabecalho;
