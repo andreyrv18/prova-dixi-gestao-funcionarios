dbc2eeafb66e0324d86c7448d4fa104d393890c9
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:26:25 -0300

    feat: (Frontend) Interfaces - novas interfaces e renomeia para seguir o padrão

--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -1,14 +1,30 @@
-export interface Funcionarios {
+export interface IFuncionarios {
     cpf: string;
     nome: string;
 }
 
-export interface Cargos {
+export interface ICargos {
     codigoDoCargo: number;
     descricaoDoCargo: string;
 }
 
-export interface Departamentos {
+export interface IDepartamentos {
     codigoDepartamento: number;
     descricaoDoDepartamento: string;
+}
+
+export interface IPageResponse<T> {
+    content: T[];
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+}
+
+export interface IVinculos {
+    cargo: string;
+    departamento: string;
+    empresa: string;
+    funicionario: string;
+    matricula: number;
 }
