328cc7152b73d7cc22f1d04b871c1ab37731508c
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:24:56 -0300

    feat: (Frontend) Pages - nota tela de Erro, agora mostra os erros vindos da api rest

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,20 +1,70 @@
-import imagem from "../assets/404.webp";
-import { Link } from "react-router-dom";
+import {isRouteErrorResponse, Link, useRouteError} from "react-router-dom";
+
+interface ApiError {
+    timestamp: string;
+    status: number;
+    error?: string;
+    message: string;
+    path: string;
+}
 
 function ErrorPage() {
+    const error = useRouteError() as ApiError;
+
+    let statusErro, mensagemErro, caminhoErro, timestampErro;
+
+    if (isRouteErrorResponse(error)) {
+        statusErro = error.status;
+        mensagemErro = error.statusText;
+    } else {
+        statusErro = error.status || 500;
+        mensagemErro = error.message;
+        caminhoErro = error.path;
+        timestampErro = error.timestamp || "";
+    }
     return (
         <div
             style={{
                 display: "grid",
                 gap: "10px",
-                fontSize: "40px",
+                fontSize: "20px", // Ajustei o tamanho da fonte geral
                 alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center", // Centraliza o texto
                 height: "75vh",
                 overflow: "hidden",
             }}
         >
-            <img src={imagem} alt="relógio ponto erro"></img>
-            <Link to="/">Voltar a página Inicial</Link>
+            {/* Exibição dos dados do erro */}
+            <p
+                style={{
+                    padding: "20px",
+                    border: "1px solid #ffcccc",
+                    borderRadius: "8px",
+                    backgroundColor: "#fff5f5",
+                }}
+            >
+                <h1 style={{ fontSize: "80px", margin: "0", color: "#d32f2f" }}>
+                    {statusErro}
+                </h1>
+                <h2 style={{ color: "#333" }}>{mensagemErro}</h2>
+
+                <p style={{ color: "#666", fontSize: "16px" }}>
+                    <strong>Caminho:</strong> {caminhoErro}
+                </p>
+                <p>{timestampErro}</p>
+            </p>
+
+            <Link
+                to="/"
+                style={{
+                    color: "#0056b3",
+                    textDecoration: "none",
+                    fontWeight: "bold",
+                }}
+            >
+                Voltar à página Inicial
+            </Link>
         </div>
     );
 }
