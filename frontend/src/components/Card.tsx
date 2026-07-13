3416cb2859538fd3c5969472df7605734c0b771b
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:27:28 -0300

    fix: (Frontend) Componentes - Deixa card generico agora podendo ser usado na tela de editar

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -7,14 +7,21 @@ interface CardProps {
         textoEditar: string;
         tituloColuna1: string;
         tituloColuna2: string;
+        tituloColuna3?: string;
+        tituloColuna4?: string;
         routeId: string;
         chavesDeAcesso: string[];
-        // rotaEdicao: string;
+        rotaEdicao: string;
+        chaveId: string;
     };
     inputs?: React.ReactNode;
 }
 
-export function Card({ inputs, tabelaConfig }: PropsWithChildren<CardProps>) {
+export function Card({
+    inputs,
+    tabelaConfig,
+    children,
+}: PropsWithChildren<CardProps>) {
     return (
         <section className={style.card}>
             {inputs && <section className={style.cardInputs}>{inputs}</section>}
@@ -24,10 +31,15 @@ export function Card({ inputs, tabelaConfig }: PropsWithChildren<CardProps>) {
                     textoEditar={tabelaConfig.textoEditar}
                     tituloColuna1={tabelaConfig.tituloColuna1}
                     tituloColuna2={tabelaConfig.tituloColuna2}
+                    tituloColuna3={tabelaConfig.tituloColuna3}
+                    tituloColuna4={tabelaConfig.tituloColuna4}
                     routeId={tabelaConfig.routeId}
                     chavesDeAcesso={tabelaConfig.chavesDeAcesso}
+                    rotaEdicao={tabelaConfig.rotaEdicao}
+                    chaveId={tabelaConfig.chaveId}
                 ></Tabela>
             )}
+            {children}
         </section>
     );
 }
