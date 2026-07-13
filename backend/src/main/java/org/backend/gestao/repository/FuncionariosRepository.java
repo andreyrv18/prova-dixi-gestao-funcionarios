e3586e5ae861d9d9e03e9c4503f2676041f0ffd8
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:32:13 -0300

    Feat: (Backend)  Repository - busca funcionarios por cpf

--- backend/src/main/java/org/backend/gestao/repository/FuncionariosRepository.java
+++ backend/src/main/java/org/backend/gestao/repository/FuncionariosRepository.java
@@ -1,12 +1,18 @@
 package org.backend.gestao.repository;
 
 import org.backend.gestao.model.Funcionarios;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 public interface FuncionariosRepository extends PagingAndSortingRepository<Funcionarios, Long>,JpaRepository<Funcionarios, Long> {
 
   boolean existsByCpf(String CPF);
 
+
+    Funcionarios findByCpf(String cpf);
 
 }
