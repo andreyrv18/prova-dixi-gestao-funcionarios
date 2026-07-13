f1e67a9f8a71e771f564c53f38d785187094b0b7
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 02:23:39 -0300

    feat: (Frontend) Util -  novas rotas da API, e corrige nome rotas do front

--- frontend/src/util/rotas.ts
+++ frontend/src/util/rotas.ts
@@ -3,40 +3,52 @@ export const API_BASE_URL = "http://localhost:8080/api";
 interface rotasProps {
     api: {
         funcionarios: string;
+        funcionariosPage: string;
         cargos: string;
         departamentos: string;
+        vinculos: string;
+        vinculosPage: string;
     };
     funcionarios: {
         listar: string;
         editar: string;
+        id: string;
     };
     cargos: {
         listar: string;
         editar: string;
+        id: string;
     };
     departamentos: {
         listar: string;
         editar: string;
+        id: string;
     };
 }
 
 export const rotas: rotasProps = {
     api: {
         funcionarios: `${API_BASE_URL}/funcionarios`,
+        funcionariosPage: `${API_BASE_URL}/funcionarios?page=0&size=10&sort=id`,
         cargos: `${API_BASE_URL}/cargos`,
         departamentos: `${API_BASE_URL}/departamentos`,
+        vinculos: `${API_BASE_URL}/vinculos`,
+        vinculosPage: `${API_BASE_URL}/vinculos?page=0&size=10&sort=id`,
     },
     funcionarios: {
         listar: "/funcionarios",
         editar: "/funcionarios/editar",
+        id: "/funcionarios/:id",
     },
     cargos: {
-        listar: "/cargo",
-        editar: "/cargo/editar",
+        listar: "/cargos",
+        editar: "/cargos/editar",
+        id: "/cargos/editar/:id",
     },
 
     departamentos: {
-        listar: "/departamento",
-        editar: "/departamento/editar",
+        listar: "/departamentos",
+        editar: "/departamentos/editar",
+        id: "/departamentos/editar/:id",
     },
 };
