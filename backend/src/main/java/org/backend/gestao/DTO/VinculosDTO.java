605d4aeb39dcb1a2eaae07876a38781088ad797f
Andreyrvdev <andreyrv18@gmail.com>
2026-07-13 23:13:19 -0300

    fix: (Backend) DTO ajusta para receber no front

--- backend/src/main/java/org/backend/gestao/DTO/VinculosDTO.java
+++ backend/src/main/java/org/backend/gestao/DTO/VinculosDTO.java
@@ -1,30 +1,55 @@
 package org.backend.gestao.DTO;
 
-import org.backend.gestao.model.Cargos;
 import org.backend.gestao.model.Vinculo;
 
 import java.io.Serializable;
 
 public class VinculosDTO  implements Serializable {
+    private Long id;
+    private String funcionario;
+    private String cpfFuncionario;
     private String empresa;
     private String matricula;
 
 
     private String cargo;
+    private Long cargoId;
     private String departamento;
-    private String funcionario;
+    private Long departamentoId;
 
     public VinculosDTO(){
 
     }
 
     public VinculosDTO(Vinculo vinculo) {
+        this.id = vinculo.getId();
         this.empresa = vinculo.getEmpresa();
         this.matricula = vinculo.getMatricula();
 
         this.cargo = vinculo.getCargo().getDescricaoDoCargo();
+        this.cargoId = vinculo.getCargo().getId();
+
         this.departamento = vinculo.getDepartamento().getDescricaoDoDepartamento();
+        this.departamentoId = vinculo.getDepartamento().getId();
+
         this.funcionario = vinculo.getFuncionario().getNome();
+    }
+    public String getCpfFuncionario() { return cpfFuncionario; }
+    public void setCpfFuncionario(String cpfFuncionario) { this.cpfFuncionario = cpfFuncionario; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(String funcionario) {
+        this.funcionario = funcionario;
     }
 
     public String getEmpresa() {
@@ -43,13 +68,20 @@ public class VinculosDTO  implements Serializable {
         this.matricula = matricula;
     }
 
-
     public String getCargo() {
         return cargo;
     }
 
     public void setCargo(String cargo) {
         this.cargo = cargo;
+    }
+
+    public Long getCargoId() {
+        return cargoId;
+    }
+
+    public void setCargoId(Long cargoId) {
+        this.cargoId = cargoId;
     }
 
     public String getDepartamento() {
@@ -60,11 +92,11 @@ public class VinculosDTO  implements Serializable {
         this.departamento = departamento;
     }
 
-    public String getFuncionario() {
-        return funcionario;
+    public Long getDepartamentoId() {
+        return departamentoId;
     }
 
-    public void setFuncionario(String funcionario) {
-        this.funcionario = funcionario;
+    public void setDepartamentoId(Long departamentoId) {
+        this.departamentoId = departamentoId;
     }
 }
